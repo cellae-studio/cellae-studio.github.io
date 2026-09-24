c000ad04eb0a6042afa87afe0338a759124a0f91
emrekayik <emrekayik1905@gmail.com>
2026-09-25 00:25:14 +0300

    refactor: update brand-pattern ambient background glow width to md:w-150

--- src/components/global/brand-pattern.tsx
+++ src/components/global/brand-pattern.tsx
@@ -46,7 +46,7 @@ export function BrandPattern() {
     >
       {/* Ambient background glow in the SVG brand color */}
       <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-10">
-        <div className="h-44 w-96 max-w-full rounded-full bg-[#315B8C]/15 blur-[100px] md:h-64 md:w-[600px] md:blur-[130px] dark:bg-[#315B8C]/25" />
+        <div className="h-44 w-96 max-w-full rounded-full bg-[#315B8C]/15 blur-[100px] md:h-64 md:w-150 md:blur-[130px] dark:bg-[#315B8C]/25" />
       </div>
 
       {/* Full-width sliding SVG track (strictly clipped by overflow-hidden) */}
